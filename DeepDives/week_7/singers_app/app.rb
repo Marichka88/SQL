bbdd727bdbf5ed14a9b11fc30df4cb56f98de7ad
MARIYA ELIYASEVYCH <meliyasevych@gmail.com>
2019-10-02 15:32:01 -0500

    commit 2

--- DeepDives/week_7/singers_app/app.rb
+++ DeepDives/week_7/singers_app/app.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
+require 'sinatra/reloader'
 
 get "/fly" do 
-  "Come Fly with Me"
+  "<h1>Come Fly with Me</h1>
+  <p>Come fly with me, we'll fly, we'll fly away</p>
+  <p>If you can use some exotic booze</p>
+  <p>There's a bar in far Bombay</p>
+  <p>Come on fly with me, we'll fly, we'll fly away</p>"
 end
