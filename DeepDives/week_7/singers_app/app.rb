15dddbc7bf6774ed9f0aee31dbede46aa6e73266
MARIYA ELIYASEVYCH <meliyasevych@gmail.com>
2019-10-02 15:39:36 -0500

    commit 3

--- DeepDives/week_7/singers_app/app.rb
+++ DeepDives/week_7/singers_app/app.rb
@@ -1,10 +1,34 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+get '/' do 
+  "<h1>Song List</h1>
+  <ul>
+    <li><a href=\"/kick\">I Get A Kick Out Of You</a></li>
+    <li><a href=\"/fly\">Come Fly with Me</a></li>
+  </ul>"
+end
+
 get "/fly" do 
-  "<h1>Come Fly with Me</h1>
+  "<a href=\"/\">Index</a>
+  <h1>Come Fly with Me</h1>
   <p>Come fly with me, we'll fly, we'll fly away</p>
   <p>If you can use some exotic booze</p>
   <p>There's a bar in far Bombay</p>
   <p>Come on fly with me, we'll fly, we'll fly away</p>"
+end
+
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do 
+  erb :index
+end
+
+get "/fly" do 
+  erb :fly
+end
+
+get '/kick' do
+  erb :kick
 end
